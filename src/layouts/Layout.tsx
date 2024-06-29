824e9e13b1e08fc78730366106b5aa1dc5b46ff3
iVeluu <velualex10@gmail.com>
2024-06-29 15:30:29 -0600

    add redirect after create new product

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -6,13 +6,13 @@ export default function Layout() {
         <>
             <header className="bg-slate-800">
                 <div className="mx-auto max-w-6xl py-10 ">
-                    <h1 className="text-4xl font-extrabold text-white">
+                    <h1 className="text-4xl font-extrabold text-white text-center md:text-left">
                         Administrador de Productos
                     </h1>
                 </div>
             </header>
             
-            <main className="mt-10 mx-auto max-w-6xl p-10 bg-white shadow">
+            <main className="mt-10 mx-auto max-w-6xl p-10 bg-white shadow ">
                 <Outlet />
             </main>
         </>
